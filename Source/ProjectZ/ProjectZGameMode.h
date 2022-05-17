e389e8e9269890d768d9db84b98f48152df31906
RoyalPizza <68163537+RoyalPizza@users.noreply.github.com>
2022-05-17 08:06:03 -0400

    Base Commit

---
+++ Source/ProjectZ/ProjectZGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectZGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectZGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectZGameMode();
+};
+
+
+
